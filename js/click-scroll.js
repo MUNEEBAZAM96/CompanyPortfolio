3ebf27d995bcb8d5b35f6355d25974608629d80b
MUNEEBAZAM96 <118824843+MUNEEBAZAM96@users.noreply.github.com>
2024-11-09 19:11:47 +0500

    Add files via upload
    This is whole project initial addition

---
+++ js/click-scroll.js
@@ -0,0 +1,41 @@
+//jquery-click-scroll
+//by syamsul'isul' Arifin
+
+var sectionArray = [1, 2, 3, 4, 5];
+<<<<<<< Tabnine <<<<<<<
+         $('.navbar-nav .nav-item .nav-link').eq(index).fadeIn(300).addClass('active');//+
+         $('.navbar-nav .nav-item .nav-link').eq(index).fadeOut(300).removeClass('inactive');//+
+>>>>>>> Tabnine >>>>>>>// {"conversationId":"9087bfff-0ff1-48ac-9a8f-41342664a5cd","source":"instruct"}
+
+$.each(sectionArray, function(index, value){
+          
+     $(document).scroll(function(){
+         var offsetSection = $('#' + 'section_' + value).offset().top - 90;
+         var docScroll = $(document).scrollTop();
+         var docScroll1 = docScroll + 1;
+         
+        
+         if ( docScroll1 >= offsetSection ){
+             $('.navbar-nav .nav-item .nav-link').removeClass('active');
+             $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
+             $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
+             $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
+         }
+         
+     });
+    
+    $('.click-scroll').eq(index).click(function(e){
+        var offsetClick = $('#' + 'section_' + value).offset().top - 90;
+        e.preventDefault();
+        $('html, body').animate({
+            'scrollTop':offsetClick
+        }, 300)
+    });
+    
+});
+
+$(document).ready(function(){
+    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');    
+    $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active');
+    $('.navbar-nav .nav-item .nav-link:link').eq(0).removeClass('inactive');
+});
